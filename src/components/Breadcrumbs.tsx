ba3ed5c1c374c3f49d3dd64c945e95dc7e06006e
Yuvraj Singh <ysgaur9919@gmail.com>
2022-08-11 18:31:07 +0530

    create: added breadcrums

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -2,27 +2,41 @@ import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react";
 import React from "react";
 import { BsChevronRight } from "react-icons/bs";
 import { TbHome } from "react-icons/tb";
+import { useLocation } from "react-router-dom";
 
 const Breadcrumbs = () => {
+  const paths = useLocation().pathname.split("/");
   return (
     <Breadcrumb
       spacing="8px"
       separator={<BsChevronRight color="gray.500" />}
-      className="w-full p-4 border border-x-0"
+      className="w-full h-16 p-4 border border-x-0 flex items-center justify-start"
     >
       <BreadcrumbItem>
-        <BreadcrumbLink href="/">
-          <TbHome />
-        </BreadcrumbLink>
+        <TbHome />
+      </BreadcrumbItem>
+      <BreadcrumbItem>
+        <BreadcrumbLink href="/">BUCKETS</BreadcrumbLink>
       </BreadcrumbItem>
 
+      {paths.map((path, index) => {
+        if (index === 0) return <></>;
+        return (
+          <BreadcrumbItem>
+            <BreadcrumbLink href={paths.slice(paths.length - index).join("/")}>
+              {path.toUpperCase()}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        );
+      })}
+      {/* 
       <BreadcrumbItem>
         <BreadcrumbLink href="/">Buckets</BreadcrumbLink>
       </BreadcrumbItem>
 
       <BreadcrumbItem isCurrentPage>
         <BreadcrumbLink href="#">Contact</BreadcrumbLink>
-      </BreadcrumbItem>
+      </BreadcrumbItem> */}
     </Breadcrumb>
   );
 };
